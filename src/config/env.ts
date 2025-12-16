75b1d1f1d03be7e7110b7fd1f551adda3eaf93ed
SOURAV-RK <souravrk25@gmail.com>
2025-12-16 13:17:05 +0530

    corrected env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,12 +4,12 @@ dotenv.config();
 
 export const ENV = {
   PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
+  MONGO_URI: process.env.MONGO_URI||"",
+  JWT_SECRET: process.env.JWT_SECRET||"",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "15m",
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET||"",
   REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN || "7d",
   NODE_ENV: process.env.NODE_ENV || "development",
-  EMAIL_USER: process.env.EMAIL_USER,
-  EMAIL_PASS: process.env.EMAIL_PASS,
+  EMAIL_USER: process.env.EMAIL_USER||"",
+  EMAIL_PASS: process.env.EMAIL_PASS||"",
 };
